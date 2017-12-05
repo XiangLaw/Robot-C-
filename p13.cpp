51a20f7e09def705ede0786d6c5b2c03f414db30
Luo Xiang <33339892+XiangLaw@users.noreply.github.com>
2017-12-05 15:03:45 +0800

    Add files via upload

---
+++ p13.cpp
@@ -0,0 +1,42 @@
+#include<iostream>
+
+int main()
+{
+	using namespace std;
+	cout<<"Enter the row of matrix p : ";// 输入矩阵行和列
+	int row;
+	cin>>row;
+	cout<<"Enter the col of matrix p : ";
+	int col;
+	cin>>col;
+	int** p=new int* [row];
+	for (int i=0;i<row;i++)
+		p[i]=new int[col];
+	for (int i=0;i<row;i++)             //输入矩阵P的元素
+		for (int j=0;j<col;j++)
+			cin>>p[i][j];
+	double sum=0.0;
+	for (int ii=0;ii<row;ii++)          //求和
+		for (int jj=0;jj<col;jj++)
+			sum=sum+p[ii][jj];
+	double mean;                       
+	mean=sum/(row*col);                  //计算期望
+	double m=0.0;
+	double variance;
+	for (int i=0;i<row;i++)              //计算方差
+		for (int j=0;j<col;j++)
+			m=m+(p[i][j]-mean)*(p[i][j]-mean);
+	variance=m/(row*col);
+	cout<<"The Expectation of matrix p: "<<mean<<endl;
+	cout<<"The Variance of matrix p: "<<variance<<endl;
+	cout<<"The Standard Deviation of matrix p: "<<sqrt(variance)<<endl;
+	for (int i=0;i<row;i++)              
+	{
+		delete [] p[i];
+		p[i]=NULL;
+	}
+	delete [] p;
+	p=NULL;
+	return 0;
+}
+
